24a91e14058f7cca42e9814ff5e35e66b9dc0b44
Jesse Bibee <jesse.bibee@gmail.com>
2015-11-09 21:10:21 -0800

    Fix integration tests

--- test/integration.js
+++ test/integration.js
@@ -10,7 +10,12 @@ before(() => pickups = deliveriesService.getPickupLocations(deliveries));
 
 describe('Pickups recommender', () => {
     it('can recommend pickups by day', () => {
-        var values = recommender.byDay(pickups, [-77.392502, 38.983965]);
-        console.log(values);
+        var days = recommender.byDay(pickups, [[-77.392502, 38.983965]]);
+        days.should.have.length.above(0);
+    });
+
+    it('can recommend pickups by location', () => {
+        var locations = recommender.byPickupLocation(pickups, [[-77.392502, 38.983965]]);
+        locations.should.have.length.above(0);
     });
 });
